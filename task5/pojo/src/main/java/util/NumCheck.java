3f9332ae308c5ba01cf2ae97e9a4fd744f3f0080
peaceman216 <2825643128@qq.com>
2019-05-13 09:35:59 +0800

    task5 完成

--- task5/pojo/src/main/java/util/NumCheck.java
+++ task5/pojo/src/main/java/util/NumCheck.java
@@ -1,4 +1,35 @@
 package util;
 
 public class NumCheck {
+    public static boolean checkNumber(double value){
+        String str = String.valueOf(value);
+        String regex = "^(-?[1-9]\\d*\\.?\\d*)|(-?0\\.\\d*[1-9])|(-?[0])|(-?[0]\\.\\d*)$";
+        return str.matches(regex);
+    }
+
+    public static boolean checkNumber(int value){
+        String str = String.valueOf(value);
+        String regex = "^(-?[1-9]\\d*\\.?\\d*)|(-?0\\.\\d*[1-9])|(-?[0])|(-?[0]\\.\\d*)$";
+        return str.matches(regex);
+    }
+
+    public static boolean checkNumber(String value){
+        String regex = "^(-?[1-9]\\d*\\.?\\d*)|(-?0\\.\\d*[1-9])|(-?[0])|(-?[0]\\.\\d*)$";
+        return value.matches(regex);
+    }
+
+
+//        System.out.println(checkNumber(0));// true
+//        System.out.println(checkNumber(-0));// true
+//        System.out.println(checkNumber(23));// true
+//        System.out.println(checkNumber(-23));// true
+//        System.out.println(checkNumber(0.0));// true
+//        System.out.println(checkNumber(-0.0));// true
+//        System.out.println(checkNumber(23.01));// true
+//        System.out.println(checkNumber(-23.01));// true
+//        System.out.println(checkNumber("-10.01.01"));// false
+//        System.out.println(checkNumber("A110"));// false
+
+
+
 }
